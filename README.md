fac5d34355183e4d07cb7d1a154be9dd396e0ea0
Niedja Araujo <163071319+niedjaraujos@users.noreply.github.com>
2025-07-26 21:37:02 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,18 @@
+# PORTFÓLIO PESSOAL
+<p align="center">
+   <img src="http://img.shields.io/static/v1?label=STATUS&message=INCOMPLETO&color=GREEN&style=for-the-badge"/>
+</p>
+
+
+
+## Funcionalidades
+
+
+## Desenvolvimento
+ - Versão desktop
+ - Manipulando o DOM
+
+
+## Tecnologias Utilizadas
+ - HTML
+ - CSS
---
+++ script.js
@@ -0,0 +1,23 @@
+<<<<<<< HEAD
+const toggleTheme = document.getElementById('toggleTheme');
+const rootHtml = document.documentElement; //pega todo o html
+
+function changeTheme() {
+  rootHtml.dataset.theme = rootHtml.dataset.theme === 'dark' ? 'light' : 'dark';
+
+  toggleTheme.classList.toggle('la-sun');
+  toggleTheme.classList.toggle('la-moon');
+}
+toggleTheme.addEventListener('click', changeTheme);
+=======
+const toggleTheme = document.getElementById('toggleTheme');
+const rootHtml = document.documentElement; //pega todo o html
+
+function changeTheme() {
+  rootHtml.dataset.theme = rootHtml.dataset.theme === 'dark' ? 'light' : 'dark';
+
+  toggleTheme.classList.toggle('la-sun');
+  toggleTheme.classList.toggle('la-moon');
+}
+toggleTheme.addEventListener('click', changeTheme);
+>>>>>>> c3459fa21e001f406303dfcec5fd7db0c42a135f
