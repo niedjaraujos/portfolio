19f4826e790056e80ea558324d0af3b2ec884b0c
niedjaraujos <naraujos1007@hotmail.com>
2025-08-16 18:28:57 -0300

    fix aside

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const toggleTheme = document.getElementById('toggleTheme');
 const rootHtml = document.documentElement; //pega todo o html
 
@@ -9,15 +8,3 @@ function changeTheme() {
   toggleTheme.classList.toggle('la-moon');
 }
 toggleTheme.addEventListener('click', changeTheme);
-=======
-const toggleTheme = document.getElementById('toggleTheme');
-const rootHtml = document.documentElement; //pega todo o html
-
-function changeTheme() {
-  rootHtml.dataset.theme = rootHtml.dataset.theme === 'dark' ? 'light' : 'dark';
-
-  toggleTheme.classList.toggle('la-sun');
-  toggleTheme.classList.toggle('la-moon');
-}
-toggleTheme.addEventListener('click', changeTheme);
->>>>>>> c3459fa21e001f406303dfcec5fd7db0c42a135f
